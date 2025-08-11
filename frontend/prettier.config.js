32fcd5b06fe58defcc26655382010cd4c8eba03f
mastetdarkpool <mastetdarkpool@gmail.com>
2025-08-11 16:59:54 +0000

    add. ESLint and Prettier configuration

---
+++ frontend/prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
